f5db5bcd27e9a646b0cb5f5b4d8070e9bfca1429
Yan Malashonak <yan.malashonak@gmail.com>
2023-03-27 18:13:48 +0300

    feat(routes/Main): implement Main route component

--- forum-app/client/src/features/layout/Main/Main.tsx
+++ forum-app/client/src/features/layout/Main/Main.tsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { ThreadCard } from 'common/components';
+import { Category, Thread } from 'model';
+import { getCategoryById, getThreadsByCategoryId } from 'services';
+
+import './Main.scss';
+
 export const Main = () => {
-  return <main className="content">Main</main>;
+  const { categoryId } = useParams();
+
+  const [category, setCategory] = useState<Category | undefined>();
+  const [threads, setThreads] = useState<Thread[] | undefined>();
+
+  useEffect(() => {
+    if (categoryId) {
+      (async () => {
+        const fetchedCategory = await getCategoryById(categoryId);
+        setCategory(fetchedCategory);
+
+        const fetchedThreads = await getThreadsByCategoryId(categoryId);
+        setThreads(fetchedThreads);
+      })();
+    } else {
+      setCategory(undefined);
+      setThreads(undefined);
+    }
+  }, [categoryId]);
+
+  return (
+    <main className="content">
+      {threads?.map(
+        (thread: Thread): JSX.Element => (
+          <ThreadCard key={thread.id} thread={thread} />
+        ),
+      )}
+    </main>
+  );
 };
