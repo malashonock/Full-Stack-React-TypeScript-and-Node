62b844c083698771325fc383b5beb2805572ec54
Yan Malashonak <yan.malashonak@gmail.com>
2023-03-28 10:58:11 +0300

    refactor(Nav): use forMobile HOC

--- forum-app/client/src/features/layout/Nav/Nav.tsx
+++ forum-app/client/src/features/layout/Nav/Nav.tsx
@@ -1,25 +1,23 @@
 import { useState } from 'react';
 
 import { MobileMenu, BurgerButton } from 'features/layout';
-import { useWindowDimensions } from 'hooks';
+import { forMobile } from 'common/hocs';
 
 import './Nav.scss';
 
+const BurgerButtonMobile = forMobile(BurgerButton);
+
 export const Nav = () => {
-  const { width } = useWindowDimensions();
   const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   const toggleMobileMenu = () => setShowMobileMenu(!showMobileMenu);
   const closeMobileMenu = () => setShowMobileMenu(false);
 
-  const mobileMenuButton =
-    width <= 768 ? <BurgerButton onClick={toggleMobileMenu} /> : null;
-
   return (
     <>
       <MobileMenu isOpen={showMobileMenu} onClose={closeMobileMenu} />
       <nav className="navigation">
-        {mobileMenuButton}
+        <BurgerButtonMobile onClick={toggleMobileMenu} />
         <strong>SuperForum</strong>
       </nav>
     </>
