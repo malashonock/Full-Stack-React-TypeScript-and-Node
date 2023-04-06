9de044ba3bfd7d0f500aef6cea96d432773d2167
Yan Malashonak <yan.malashonak@gmail.com>
2023-04-06 17:44:02 +0300

    feat(client/routes/userprofile): implement UserProfilePage

--- forum-app/client/src/pages/UserProfilePage/UserProfilePage.tsx
+++ forum-app/client/src/pages/UserProfilePage/UserProfilePage.tsx
@@ -1,3 +1,51 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { SectionDivider } from 'common/components/layout';
+import {
+  ChangePasswordForm,
+  UserThreadCommentsList,
+  UserThreadsList,
+} from './components';
+
+import './UserProfilePage.scss';
+import { Thread, ThreadItem } from 'model';
+import { getUserThreadComments, getUserThreads } from 'services';
+
 export const UserProfilePage = () => {
-  return <div>User profile page</div>;
+  const { userId } = useParams();
+
+  const [userThreads, setUserThreads] = useState<Thread[]>([]);
+  const [userThreadComments, setUserThreadComments] = useState<ThreadItem[]>(
+    [],
+  );
+
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+
+    (async () => {
+      const fetchedUserThreads = await getUserThreads(userId);
+      setUserThreads(fetchedUserThreads);
+
+      const fetchedUserThreadComments = await getUserThreadComments(userId);
+      setUserThreadComments(fetchedUserThreadComments);
+    })();
+  }, [userId]);
+
+  return (
+    <div className="user-profile">
+      <div className="user-profile__change-password">
+        <ChangePasswordForm />
+      </div>
+      <SectionDivider />
+      <div className="user-profile__user-threads">
+        <UserThreadsList threads={userThreads} />
+      </div>
+      <div className="user-profile__user-thread-comments">
+        <UserThreadCommentsList threadComments={userThreadComments} />
+      </div>
+    </div>
+  );
 };
