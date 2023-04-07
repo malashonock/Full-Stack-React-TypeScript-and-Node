35043b6f966a8fc0746052d4253dc1489bc14b66
Yan Malashonak <yan.malashonak@gmail.com>
2023-04-07 15:23:38 +0300

    feat(server): use Redis middleware & router

--- forum-app/node-server/src/index.ts
+++ forum-app/node-server/src/index.ts
@@ -1,10 +1,27 @@
+import path from 'path';
+import * as dotenv from 'dotenv';
+
+// Read variables from .env
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+});
+console.log(process.env.NODE_ENV);
+
 import express from 'express';
 import { createServer } from 'http';
+import sessionMiddleware from './middlewares/session';
+import router from './routes/index';
 
+// Setup Express server
 const app = express();
-
 const server = createServer(app);
 
-server.listen({ port: 8000 }, () => {
+// Set up middlewares
+app.use(sessionMiddleware);
+
+// Set up routes
+app.use(router);
+
+server.listen({ port: process.env.SERVER_PORT }, () => {
   console.log('Server is running!');
 });
