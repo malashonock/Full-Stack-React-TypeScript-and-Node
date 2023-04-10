84d4d23cce54373f6464faf2ad238ae4ceda7163
Yan Malashonak <yan.malashonak@gmail.com>
2023-04-10 19:38:34 +0300

    refactor(server/routes): rename index route

--- forum-app/node-server/src/routes/index.route.ts
+++ forum-app/node-server/src/routes/index.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import indexController from '../controllers/index';
+import indexController from '../controllers/index.controller';
 
 const router = express.Router();
 
