656fc426d761a85ae673095ab6c3e8d84ebfcafc
Yan Malashonak <yan.malashonak@gmail.com>
2023-04-11 19:37:58 +0300

    refactor(server/repo): return full User from user repo

--- forum-app/node-server/src/controllers/user.controller.ts
+++ forum-app/node-server/src/controllers/user.controller.ts
@@ -6,7 +6,13 @@ const registerUser: RequestHandler = async (req, res) => {
   try {
     const { userName, email, password } = req.body;
     const createdUser = await UserRepo.createUser(userName, email, password);
-    res.send(createdUser);
+    const { id } = createdUser;
+    
+    res.send({
+      id,
+      userName,
+      email,
+    });
   } catch (error) {
     res.status(500).send((error as Error).message);
   }
