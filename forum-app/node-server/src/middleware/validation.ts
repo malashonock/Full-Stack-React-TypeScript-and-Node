5cfb3a37b2833002a6eb2a69fa279c78edc2fac5
Yan Malashonak <yan.malashonak@gmail.com>
2023-04-14 15:52:10 +0300

    feat(server/middleware/validation): implement validateUpdateUser

--- forum-app/node-server/src/middleware/validation.ts
+++ forum-app/node-server/src/middleware/validation.ts
@@ -1,11 +1,11 @@
 import { RequestHandler } from 'express';
-import { NewUserFields } from '../shared/dto/User.dto';
+import { UserFields } from '../shared/dto/User.dto';
 import ValidationService from '../services/validation.service';
 import { containsDigits, containsLowercaseLetters, containsSpecialChars, containsUppercaseLetters, isLongEnough, isRequired, isValidEmail } from '../shared/validation/validators';
 import { FormValidationSchema } from '../shared/validation/types';
 
 export const validateNewUser: RequestHandler = (req, res, next) => {
-  const validationResult = ValidationService.runValidators(req.body as NewUserFields, {
+  const validationResult = ValidationService.runValidators(req.body as UserFields, {
     userName: isRequired,
     email: [isRequired, isValidEmail],
     password: [
@@ -16,7 +16,28 @@ export const validateNewUser: RequestHandler = (req, res, next) => {
       containsDigits,
       containsSpecialChars,
     ],
-  } as FormValidationSchema<NewUserFields>);
+  } as FormValidationSchema<UserFields>);
+
+  if (validationResult.errors) {
+    res.status(400).send(validationResult.errors);
+  } else {
+    next();
+  }
+};
+
+export const validateUpdatedUser: RequestHandler = (req, res, next) => {
+  const validationResult = ValidationService.runValidators(req.body as UserFields, {
+    userName: req.body.userName ? isRequired : null,
+    email: req.body.email ? [isRequired, isValidEmail] : null,
+    password: req.body.password ? [
+      isRequired, 
+      isLongEnough, 
+      containsUppercaseLetters,
+      containsLowercaseLetters,
+      containsDigits,
+      containsSpecialChars,
+    ] : null,
+  } as FormValidationSchema<UserFields>);
 
   if (validationResult.errors) {
     res.status(400).send(validationResult.errors);
