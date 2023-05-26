f158b58da63a1da815fc495de1a40f4187e7c01e
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-26 13:49:31 +0300

    feat(server/middleware): add isAuthorized middleware

--- forum-app/packages/rest-server/src/middleware/auth.ts
+++ forum-app/packages/rest-server/src/middleware/auth.ts
@@ -2,8 +2,25 @@ import { RequestHandler } from 'express';
 
 export const isAuthenticated: RequestHandler = (req, res, next) => {
   if (!req.session.userId) {
-    res.status(401).send('You need to log in first to perform this request');
-  } else {
-    next();
+    return res
+      .status(401)
+      .send('You need to log in first to perform this request');
   }
+
+  next();
+};
+
+export const isAuthorized: RequestHandler = (req, res, next) => {
+  const { userId } = req.params;
+  if (!userId) {
+    return res.status(400).send('User id is not specified');
+  }
+
+  if (req.session.userId !== userId) {
+    return res
+      .status(403)
+      .send('User is not authorized to perform this request');
+  }
+
+  next();
 };
